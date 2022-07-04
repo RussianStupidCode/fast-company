6e1df69d22f42713590ba66ef0e064c726653ea0
RussianStupidCode <67269808+RussianStupidCode@users.noreply.github.com>
2022-07-04 16:56:56 +0000

    Deploying to gh-pages from @ RussianStupidCode/fast-company@0c10b39313e93e657ec1436c4ac4ce531f9aa9f0 🚀

---
+++ precache-manifest.711dbe15a360a4247b9ecd09faf4ede5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "d6845b595d91f0d93b78",
+    "url": "/static/css/main.f5931d6e.chunk.css"
+  },
+  {
+    "revision": "d6845b595d91f0d93b78",
+    "url": "/static/js/main.d6845b59.chunk.js"
+  },
+  {
+    "revision": "03ae7ea834bbe7645a20",
+    "url": "/static/js/1.03ae7ea8.chunk.js"
+  },
+  {
+    "revision": "ed1b4e0f7217231c835f",
+    "url": "/static/css/2.cd21544b.chunk.css"
+  },
+  {
+    "revision": "ed1b4e0f7217231c835f",
+    "url": "/static/js/2.ed1b4e0f.chunk.js"
+  },
+  {
+    "revision": "8ba3075663cac21e8fc5",
+    "url": "/static/js/runtime~main.8ba30756.js"
+  },
+  {
+    "revision": "11ad1ed5281e0fb1e7de25a267d0f3f5",
+    "url": "/index.html"
+  }
+];
